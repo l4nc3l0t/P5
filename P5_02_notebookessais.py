7e1eb90871940e6a25fd0ec45212faa1737265a5
lancelot <lancelot.leclercq@free.fr>
2022-01-07 13:34:00 +0100

    réorga visu data/kmeans

--- P5_02_notebookessais.py
+++ P5_02_notebookessais.py
@@ -1,4 +1,5 @@
 # %%
+import os
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -35,55 +36,9 @@ else:
     pas de création de figures ni de tableaux""")
 
 # %%
-OlistData = pd.read_csv('OlistData.csv')
+DataRFM = pd.read_csv('OlistDataRFM.csv')
 # %%
-DataRFM = OlistData[['last_purchase_days', 'orders_number',
-                     'mean_payement']].dropna()
 ScaledData = StandardScaler().fit_transform(DataRFM)
-
-# %%
-# MDS sur les 500 premières lignes
-stress = []
-for n_comp in range(2, 13):
-    mds = MDS(n_components=n_comp, n_jobs=-1)
-    ScaledData_fit = mds.fit(ScaledData[:500])
-    stress.append(mds.stress_)
-# %%
-fig = px.line(x=range(2, 13),
-              y=stress,
-              labels={
-                  'x': 'n_components',
-                  'y': 'stress'
-              })
-fig.show(renderer='notebook')
-# %%
-mds3 = MDS(n_components=4, n_jobs=-1)
-ScaledData_fit3 = mds3.fit(ScaledData[:500]).embedding_
-# %%
-fig = px.scatter_3d(ScaledData_fit3, x=0, y=1, z=2, color=3)
-fig.show(renderer='notebook')
-
-
-# %%
-distortions = []
-silhouettes = []
-for i in range(2, 23):
-    km = KMeans(n_clusters=i)
-    km.fit(ScaledData)
-    Clusters = km.predict(ScaledData)
-    distortions.append(km.inertia_)
-    silhouettes.append(silhouette_score(ScaledData, Clusters))
-
-# %%
-fig = make_subplots(specs=[[{'secondary_y': True}]])
-fig.add_trace(go.Scatter(x=[*range(2, 23)], y=distortions, name='distortion'),
-              secondary_y=False)
-fig.add_trace(go.Scatter(x=[*range(2, 23)], y=silhouettes, name='silhouette'),
-              secondary_y=True)
-fig.update_xaxes(title_text='n_clusters')
-fig.update_yaxes(title_text="distortion", secondary_y=False)
-fig.update_yaxes(title_text="silhouette", secondary_y=True)
-fig.show(renderer='notebook')
 # %%
 # ACP
 pca = PCA().fit(ScaledData)
@@ -102,32 +57,28 @@ fig = px.area(x=range(1, exp_var_cum.shape[0] + 1),
 fig.update_layout(title='Scree plot')
 fig.show(renderer='notebook')
 if write_data is True:
-    fig.write_image('./Figures/ScreePlotKMeans.pdf', height=300)
+    fig.write_image('./Figures/ScreePlotRFM.pdf', height=300)
 
+# %%
+# graph PCA 3D
+fig = px.scatter_3d(components, x=0, y=1, z=2)
+fig.show(renderer='notebook')
 # %%
 # création des graphiques
 for a1, a2 in [[0, 1], [0, 2], [1, 2]]:
-    fig = visuPCA(DataRFM,
-                  pca,
-                  components,
-                  loadings, [(a1, a2)],
-                  color=None)
+    fig = visuPCA(DataRFM, pca, components, loadings, [(a1, a2)], color=None)
     fig.show(renderer='notebook')
     if write_data is True:
-        fig.write_image('./Figures/PCAKMeansF{}F{}.pdf'.format(a1 + 1, a2 + 1),
+        fig.write_image('./Figures/PCARFMF{}F{}.pdf'.format(a1 + 1, a2 + 1),
                         width=500,
                         height=500)
+
 # %%
-# clustering KMeans
-DataClustering = KMeans(n_clusters=3).fit(ScaledData)
-ScaledData_KMeansTransformed = DataClustering.transform(ScaledData)
-Clusters = DataClustering.predict(ScaledData)
+# MDS
+mds3 = MDS(n_components=3, n_jobs=-1)
+ScaledData_transformed3 = mds3.fit(ScaledData).embedding_
 # %%
-fig = px.scatter_3d(ScaledData_KMeansTransformed,
-                 x=0,
-                 y=1,
-                 z=2,
-                 color=DataClustering.labels_)
+fig = px.scatter_3d(ScaledData_transformed3, x=0, y=1, z=2)
 fig.show(renderer='notebook')
 
 # %%
@@ -139,6 +90,52 @@ tsne = TSNE(n_components=2,
             n_jobs=-1)
 ScaledData_TSNEfit = tsne.fit_transform(ScaledData)
 # %%
-fig = px.scatter(ScaledData_TSNEfit, x=0, y=1, color=DataClustering.labels_)
+fig = px.scatter(ScaledData_TSNEfit, x=0, y=1)
 fig.show(renderer='notebook')
+
 # %%
+distortions = []
+silhouettes = []
+for i in range(2, 13):
+    km = KMeans(n_clusters=i)
+    km.fit(ScaledData)
+    Clusters = km.predict(ScaledData)
+    distortions.append(km.inertia_)
+    silhouettes.append(silhouette_score(ScaledData, Clusters))
+
+# %%
+fig = make_subplots(specs=[[{'secondary_y': True}]])
+fig.add_trace(go.Scatter(x=[*range(2, 13)], y=distortions, name='distortion'),
+              secondary_y=False)
+fig.add_trace(go.Scatter(x=[*range(2, 13)], y=silhouettes, name='silhouette'),
+              secondary_y=True)
+fig.update_xaxes(title_text='n_clusters')
+fig.update_yaxes(title_text="distortion", secondary_y=False)
+fig.update_yaxes(title_text="silhouette", secondary_y=True)
+fig.show(renderer='notebook')
+
+# %%
+# clustering KMeans
+DataClustering = KMeans(n_clusters=5).fit(ScaledData)
+# %%
+fig = px.scatter_3d(DataRFM,
+                    x='last_purchase_days',
+                    y='orders_number',
+                    z='mean_payement',
+                    color=DataClustering.labels_)
+fig.show(renderer='notebook')
+
+# %%
+# TSNE
+tsne = TSNE(n_components=2,
+            perplexity=50,
+            init='pca',
+            learning_rate='auto',
+            n_jobs=-1)
+ScaledData_TSNEfit = tsne.fit_transform(ScaledData)
+# %%
+fig = px.scatter(ScaledData_TSNEfit,
+                 x=0,
+                 y=1,
+                 color=DataClustering.labels_)
+fig.show(renderer='notebook')
